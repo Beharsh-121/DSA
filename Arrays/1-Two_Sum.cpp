82297199874d03f76b7659bbebc2fe4cb956cd53
Harsh tiwari <30harsh.tiwari@gmail.com>
2025-04-15 08:41:56 +0530

    Added Brute Force for Two Sum

--- Arrays/1-Two_Sum.cpp
+++ Arrays/1-Two_Sum.cpp
@@ -1,7 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
+//Brute Force Approach
+// Time Complexity - O(N^2)
+//Space Complexity - O(1)
 
-int main(){
-
-    return 0;
+vector<int> twoSum(vector<int>& nums, int target) {
+    for(int i = 0; i < nums.size(); i++){
+        int numToFind = target - nums[i];
+        for(int j = i+1; j < nums.size(); j++){
+            if(nums[j] == numToFind) return {i, j};
+        }
+    }
+    return {-1, 1};
 }
