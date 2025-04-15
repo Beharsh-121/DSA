7cfc74ad457337f78f316f9421ec58436c45dd37
Harsh tiwari <30harsh.tiwari@gmail.com>
2025-04-15 08:51:46 +0530

    Completed The Two Sum Problem

--- Arrays/1-Two_Sum.cpp
+++ Arrays/1-Two_Sum.cpp
@@ -11,3 +11,18 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return {-1, 1};
 }
+
+
+//Better Approach
+//Time Complexity - O(N)
+//Space Complexity - O(N)
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> mp;
+    for(int i = 0; i < nums.size(); i++){
+       int numToFind = target - nums[i];
+       if(mp.find(numToFind) != mp.end()) return {mp[numToFind], i};
+       mp[nums[i]] = i;
+    }
+    return {0, 0};
+ }
